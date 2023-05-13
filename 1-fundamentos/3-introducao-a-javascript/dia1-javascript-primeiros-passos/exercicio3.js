7326054816daf273745adfe4437c552cf6ae9d78
analaurafr <laurafernandes@outlook.com>
2023-05-13 01:15:21 -0300

    resolução do exercício 3

---
+++ 1-fundamentos/3-introducao-a-javascript/dia1-javascript-primeiros-passos/exercicio3.js
@@ -0,0 +1,27 @@
+/* 3 - Utilize if/else para escrever um código que retorne o maior de três números. Defina, no começo do seu código, três variáveis com os valores que serão comparados.
+
+let a = 6;
+let b = 4;
+let c = 2;
+
+if (a > b && a > c) {
+  console.log('O maior número é: ' + a + ' (a)');
+} else if (b > a && b > c) {
+  console.log('O maior número é: ' + b + ' (b)');
+} else {
+  console.log('O maior número é: ' + c + ' (c)');
+};
+
+*/
+
+let a = 6;
+let b = 4;
+let c = 2;
+
+if (a > b && a > c) {
+    console.log('O maior número é: ' + a + ' (a)');
+} else if (b > a && b > c) {
+    console.log('O maior número é: ' + b + ' (b)');
+} else {
+    console.log('O maior número é: ' + c + ' (c)');
+};

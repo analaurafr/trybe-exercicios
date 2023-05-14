b272d3151e24086091b99f7c51165a8f6a0063bd
analaurafr <laurafernandes@outlook.com>
2023-05-14 18:55:57 -0300

    estudos sobre arrays

---
+++ 1-fundamentos/3-introducao-a-javascript/dia2-javascript-array-e-loop-for/arrays.js
@@ -0,0 +1,12 @@
+// let tasksList = ['tomar café', 'reunião', 'brincar', 'tomar banho', 'dançar'];
+
+// console.log(tasksList.length); 
+
+let tasksList = ['tomar café', 'reunião', 'brincar', 'tomar banho', 'dançar'];
+
+// let searchForFirstTask = tasksList[3];
+// console.log(searchForFirstTask);
+
+let searchForLastTask = tasksList[tasksList.length - 1];
+console.log(searchForLastTask);
+
